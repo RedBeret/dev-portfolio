183342e64e39b775d1f4f387b392c17512b57565
Steven <espisteven5@gmail.com>
2024-03-25 18:11:22 -0700

    updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,37 +1,27 @@
 import React, { Component } from "react";
 
 class Skills extends Component {
-  renderSkills(skills) {
-    return skills.map((skill, i) => (
-      <li className="list-inline-item mx-3" key={i}>
-        <span>
-          <div className="text-center skills-tile">
-            <i className={skill.class} style={{ fontSize: "220%" }}>
-              <p
-                className="text-center"
-                style={{ fontSize: "30%", marginTop: "4px" }}
-              >
-                {skill.name}
-              </p>
-            </i>
-          </div>
-        </span>
-      </li>
-    ));
-  }
-
   render() {
-    let frontendSkills, backendSkills;
     if (this.props.sharedSkills && this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.skills;
-      
-      if (this.props.sharedSkills.frontend) {
-        frontendSkills = this.renderSkills(this.props.sharedSkills.frontend);
-      }
-      
-      if (this.props.sharedSkills.backend) {
-        backendSkills = this.renderSkills(this.props.sharedSkills.backend);
-      }
+      var skills = this.props.sharedSkills.icons.map(function (skills, i) {
+        return (
+          <li className="list-inline-item mx-3" key={i}>
+            <span>
+              <div className="text-center skills-tile">
+                <i className={skills.class} style={{ fontSize: "220%" }}>
+                  <p
+                    className="text-center"
+                    style={{ fontSize: "30%", marginTop: "4px" }}
+                  >
+                    {skills.name}
+                  </p>
+                </i>
+              </div>
+            </span>
+          </li>
+        );
+      });
     }
 
     return (
@@ -43,10 +33,7 @@ class Skills extends Component {
             </h1>
           </div>
           <div className="col-md-12 text-center">
-            <h2>Frontend Skills</h2>
-            <ul className="list-inline mx-auto skill-icon">{frontendSkills}</ul>
-            <h2>Backend Skills</h2>
-            <ul className="list-inline mx-auto skill-icon">{backendSkills}</ul>
+            <ul className="list-inline mx-auto skill-icon">{skills}</ul>
           </div>
         </div>
       </section>
